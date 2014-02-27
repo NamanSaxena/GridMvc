633105fa481b58d8e468da0dfa334abc403a8765
Anton Podavalov <a.podavalov@gmail.com>
2014-02-27 22:59:30 +0400

    Bump the 0.1.3 version.

--- GridMvc/Properties/AssemblyInfo.cs
+++ GridMvc/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.2.*")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.1.3.*")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
 [assembly: InternalsVisibleTo("GridMvc.Tests")]
